0b8bd4717478f1bc0bd08eddf1ec63e2382aec20
shwetapund <pundshweta2020@gmail.com>
2026-02-13 21:03:49 +0530

    create User model

--- server/index.js
+++ server/index.js
@@ -28,14 +28,7 @@ app.get("/api/v1/healths",
 
 
 
-
-
-
-
-
-
-
-const PORT = process.env.BASE_URL || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+import { Schema, model } from mongoose;
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobileNo:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    gender:{
+        type:String
+    },
+    role:{
+        type:String,
+        default: 'user'
+    }
+})
+
+const User = model('User', UserSchema);
+
+export default User;
